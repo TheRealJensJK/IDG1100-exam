902619563590decd1e61434af6bf0cef75244dda
TheRealJensJK <72973197+TheRealJensJK@users.noreply.github.com>
2021-06-04 12:53:37 +0200

    fixed type error

--- pages/lists.js
+++ pages/lists.js
@@ -9,9 +9,9 @@ fetch('lists.json')
     for(i in data){
         var table = document.getElementById("jsonTable");
         var row = table.insertRow(0);
-        var cell1 = row.insertCell(0);
-        var cell2 = row.insertCell(1);
-        var cell3 = row.insertCell(2);
+        var cell0 = row.insertCell(0);
+        var cell1 = row.insertCell(1);
+        var cell2 = row.insertCell(2);
 
         cell0.innerHTML = data[i]["name"];
         cell1.innerHTML = data[i]["age"];
